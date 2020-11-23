b0142024eccf3ee0f6c7cf5e618d580c482e15b5
Duong Pham Thai <duong1090@gmail.com>
2020-11-23 22:58:23 +0700

    header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../login/authenticate.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Input() title = '';
+  @Input() subTitle = '';
+  @Input() button = '';
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onLogout = () => {
+    this.authenticationService.logout();
+    this.router.navigateByUrl('/login');
+  };
 }
