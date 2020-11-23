a69bed92f473e655698dba1de6c16d8abf4bf613
Duong Pham Thai <duong1090@gmail.com>
2020-11-23 22:53:44 +0700

    routing screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: '',
     component: LoginComponent,
+  },
+  { path: 'product/list', component: ProductListComponent },
+  { path: 'product/add', component: ProductAddComponent },
+  { path: 'product/detail', component: ProductDetailComponent },
+  {
+    path: 'cart',
+    component: CartComponent,
   },
 ];
 
